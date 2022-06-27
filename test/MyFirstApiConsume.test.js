93b67fb685425de5f2d712589a6ed5a2d0dab5a3
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-06-27 14:38:04 -0500

    Corrections for first api test

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -21,41 +21,29 @@ describe('First Api Tests', () => {
         expect(response.status).to.equal(StatusCodes.OK);
         expect(response.config.query).to.eql(query);
     });
-
     it('Consume HEAD Service', async () => {
         const response = await axios.head('https://httpbin.org/headers');
-
         expect(response.status).to.equal(StatusCodes.OK);
         expect(response.headers).to.have.property("content-type");
         expect(response.data).to.eql("")
-
-    })
-
+    });
     it('Consume PATCH Service', async () => {
-        const response = await axios.patch("https://httpbin.org/patch", query);
+        const response = await axios.patch("https://httpbin.org/patch",{ body });
 
         expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.eql(JSON.stringify(query))
-        expect(response.data.json).to.eql(query)
-
-    })
-
+        expect(response.data.data).to.eql(JSON.stringify(body));
+        expect(response.data.json).to.eql(body);
+    });
     it('Consume PUT Service', async () => {
-        const response = await axios.put("https://httpbin.org/put", query);
+        const response = await axios.put("https://httpbin.org/put",{ query });
 
         expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.eql(JSON.stringify(query))
-
-    })
-
+        expect(response.data.data).to.eql(JSON.stringify(query));
+    });
     it('Consume DELETE Service', async () => {
         const response = await axios.delete("https://httpbin.org/delete");
 
         expect(response.status).to.equal(StatusCodes.OK);
         expect(response.data.data).to.eql("")
-
-    })
-
-    
-
+    });
 });
