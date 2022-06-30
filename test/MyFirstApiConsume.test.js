9a2dc43bae5026fce4f9796ec5d38b8c4de98672
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-06-30 12:56:07 -0500

    segunda correcion del primer test

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
 const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
+const query = {
+    name: 'John',
+    age: '31',
+    city: 'New York'
+};
 
 describe('First Api Tests', () => {
     it('Consume GET Service', async () => {
@@ -10,11 +15,7 @@ describe('First Api Tests', () => {
         expect(response.data).to.have.property('origin');
     });
     it('Consume GET Service with query parameters', async () => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+        
 
         const response = await axios.get('https://httpbin.org/get', { query });
 
@@ -28,11 +29,11 @@ describe('First Api Tests', () => {
         expect(response.data).to.eql("")
     });
     it('Consume PATCH Service', async () => {
-        const response = await axios.patch("https://httpbin.org/patch",{ body });
+        const response = await axios.patch("https://httpbin.org/patch",{ query });
 
         expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.eql(JSON.stringify(body));
-        expect(response.data.json).to.eql(body);
+        expect(response.data.data).to.eql(JSON.stringify(query));
+        expect(response.data.json).to.eql(query);
     });
     it('Consume PUT Service', async () => {
         const response = await axios.put("https://httpbin.org/put",{ query });
