1b1aa879fb610bf56bc9695044af405bbc1c11ab
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-06-30 18:58:43 -0500

    tercer correcion api test

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
-const query = {
+const data = {
     name: 'John',
     age: '31',
     city: 'New York'
@@ -15,12 +15,10 @@ describe('First Api Tests', () => {
         expect(response.data).to.have.property('origin');
     });
     it('Consume GET Service with query parameters', async () => {
-        
-
-        const response = await axios.get('https://httpbin.org/get', { query });
+        const response = await axios.get('https://httpbin.org/get', data);
 
         expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.query).to.eql(query);
+        expect(response.config.query).to.eql(data);
     });
     it('Consume HEAD Service', async () => {
         const response = await axios.head('https://httpbin.org/headers');
@@ -29,17 +27,17 @@ describe('First Api Tests', () => {
         expect(response.data).to.eql("")
     });
     it('Consume PATCH Service', async () => {
-        const response = await axios.patch("https://httpbin.org/patch",{ query });
+        const response = await axios.patch("https://httpbin.org/patch", data );
 
         expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.eql(JSON.stringify(query));
+        expect(response.data.data).to.eql(JSON.stringify(data));
         expect(response.data.json).to.eql(query);
     });
     it('Consume PUT Service', async () => {
-        const response = await axios.put("https://httpbin.org/put",{ query });
+        const response = await axios.put("https://httpbin.org/put", data);
 
         expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.eql(JSON.stringify(query));
+        expect(response.data.json).to.eql(query);
     });
     it('Consume DELETE Service', async () => {
         const response = await axios.delete("https://httpbin.org/delete");
