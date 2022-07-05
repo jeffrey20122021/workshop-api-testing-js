401bc8cf9914647fef26fa91df0a483cdbf92116
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-07-05 13:14:27 -0500

    recall put

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -16,6 +16,13 @@ describe('idempotence', () => {
     const response = await object.get('https://api.github.com/user/following');
     expect(response.data.find((user) => user.login === 'aperdomob')).to.not.eql(undefined);
   });
+});
+describe('idempotence', () => {
+  it('Recall PUT ', async () => {
+    const response = await object.put('https://api.github.com/user/following/aperdomob');
+    expect(response.status).to.equal(204);
+    expect(response.data).to.equal('');
+  });
   it('refollowing test', async () => {
     const response = await object.get('https://api.github.com/user/following');
     expect(response.data.find((user) => user.login === 'aperdomob')).to.not.eql(undefined);
