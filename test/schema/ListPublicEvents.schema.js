d73c99a36b1d2e28dad493ad98ba857cfb57a31f
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-07-06 08:51:28 -0500

    listpublicevents

---
+++ test/schema/ListPublicEvents.schema.js
@@ -0,0 +1,36 @@
+const listPublicEventsSchema = {
+    type: 'array',
+  items: {
+    type: 'object',
+    properties: {
+      id: { type: 'string' },
+      type: { type: 'string' },
+      actor: {
+        type: 'object',
+        properties: {
+          id: { type: 'integer' },
+          login: { type: 'string' },
+          display_login: { type: 'string' },
+          gravatar_id: { type: 'string' },
+          url: { type: 'string' },
+          avatar_url: { type: 'string' }
+        }
+      },
+      repo: {
+        type: 'object',
+        properties: {
+          id: { type: 'integer' },
+          name: { type: 'string' },
+          url: { type: 'string' }
+        }
+      },
+      payload: {
+        type: 'object'
+      },
+      public: { type: 'boolean' },
+      created_at: { type: 'string' }
+    }
+  }
+};
+
+exports.listPublicEventsSchema = listPublicEventsSchema;
